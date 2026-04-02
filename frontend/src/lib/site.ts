df395886019b86cf27ba7bd26364804767ba646e
Cinyan10 <cinyan10@icloud.com>
2026-04-02 16:49:04 +0200

    chore: update rontend/src/lib/site.ts

--- frontend/src/lib/site.ts
+++ frontend/src/lib/site.ts
@@ -2,7 +2,7 @@ export const SITE_NAME = "GOKZ.TOP"
 export const SITE_START_YEAR = 2024
 
 export function getPageTitle(pageTitle?: string): string {
-  return pageTitle ? `${pageTitle} | ${SITE_NAME}` : SITE_NAME
+  return pageTitle ? `${SITE_NAME} - ${pageTitle}` : SITE_NAME
 }
 
 export function getCopyrightYearRange(year = new Date().getFullYear()): string {
